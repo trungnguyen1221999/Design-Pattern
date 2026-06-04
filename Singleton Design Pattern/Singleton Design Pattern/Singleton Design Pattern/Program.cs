c7ffc746d22b904c71b0755b494c68a5fe065aca
KAI - Trung Nguyen <trungnguyen1221999@gmail.com>
2026-06-04 08:22:34 +0300

    singleton

---
+++ Singleton Design Pattern/Singleton Design Pattern/Singleton Design Pattern/Program.cs
@@ -0,0 +1,8 @@
+﻿using Singleton_Design_Pattern;
+
+Singleton s1 = Singleton.GetSingleton();
+Singleton s2 = Singleton.GetSingleton();
+
+Console.WriteLine(s1);
+if (s1 == s2) Console.WriteLine("Singleton works, both variables contain the same instance");
+else Console.WriteLine("Singleton fails, variables contain different instance");
---
+++ Singleton Design Pattern/Singleton Design Pattern/Singleton Design Pattern/Singleton.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Singleton_Design_Pattern
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+        private static Singleton _instance;
+
+        public static Singleton GetSingleton()
+        {
+            if (_instance == null) { _instance = new Singleton(); }
+            return _instance;
+        }
+
+        public void DoSomething()
+        {
+
+        }
+    }
+}
